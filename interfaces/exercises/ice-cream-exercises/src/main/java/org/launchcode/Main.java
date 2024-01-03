b9ad2700e795ce091e720de9ddfef997679feb1d
YumanGong <gongymsilvia@gmail.com>
2024-01-03 14:23:26 -0600

    update

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -8,10 +8,34 @@ public class Main {
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
 
+        Ingredient ingredient = new Ingredient();
+        System.out.println(ingredient);
+
+
+        System.out.println("Before:");
+        for (Flavor flavor : flavors) {
+            System.out.println(flavor.getName());
+        }
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
+        FlavorComparator flavorComparator = new FlavorComparator();
+        flavors.sort(flavorComparator);
+        System.out.println("\nAfter:");
+        for (Flavor flavor : flavors) {
+            System.out.println(flavor.getName());
+        }
 
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
+        System.out.println("\nBefore:");
+        for (Cone cone:cones) {
+            System.out.println(cone.getName()+":"+cone.getCost());
+        }
+        ConeComparator coneComparator = new ConeComparator();
+        cones.sort(coneComparator);
 
+        System.out.println("\nAfter:");
+        for (Cone cone:cones) {
+            System.out.println(cone.getName()+":"+cone.getCost());
+        }
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
     }
 }
